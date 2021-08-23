28a845472a31dd9822a2595312b1f2f23a94a12e
Khadijah <khadijah99.mdaud@gmail.com>
2021-08-23 15:03:15 +0800

    chapter_9

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_085719) do
-
-  create_table "feedbacks", charset: "utf8mb3", force: :cascade do |t|
-    t.string "author"
-    t.string "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_09_10_081521) do
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
@@ -26,6 +19,9 @@ ActiveRecord::Schema.define(version: 2021_09_09_085719) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
--- test/controllers/static_pages_controller_test.rb
+++
@@ -1,6 +0,0 @@
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get static_pages_index_url
-    assert_response :success
-  end
-end
